64e2c081654d367300ebb1c33c792207399aab32
Mangaliso Nogantshi <mnogantshi@hotmail.com>
2024-06-24 23:50:18 +0200

    Added Game Board Display for Start of Game

--- hangman.py
+++ hangman.py
@@ -54,3 +54,12 @@ HANGMANPICS = ['''
 
 # choose word from word_list
 chosen_word = random.choice(word_list.WORDLIST)
+
+while True:
+    # print game board
+    print(HANGMANPICS[0] + '\n')
+
+    word_length = len(chosen_word)
+    print(*(["_"] * word_length), sep=" ")
+
+    break
