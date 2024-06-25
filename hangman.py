288adfae0b05e8af842382a14d7de558a645b205
Mangaliso Nogantshi <mnogantshi@hotmail.com>
2024-06-25 02:16:56 +0200

    added lives

--- hangman.py
+++ hangman.py
@@ -62,20 +62,26 @@ def IsValidGuess(guess: str):
      
   else:
     return True
-
-# choose word from word_list
-chosen_word = random.choice(word_list.WORDLIST)
+  
+game_over = False
 
 while True:
+    # choose word from word_list
+    chosen_word = random.choice(word_list.WORDLIST)
+
+    # number of lives
+    lives = 6
+
     # print game board
     print(HANGMANPICS[0] + '\n')
 
     word_length = len(chosen_word)
     print(*(["_"] * word_length), sep=" ")
 
-    # takes player guess
-    guessed_letter = input("Enter a letter to guess\n")
-    if not IsValidGuess(guessed_letter):
-       print("Please make valid guess.")  # implement details on why guess is invalid
-
-    break
+    while not game_over:
+      # takes player guess
+      guessed_letter = input("Enter a letter to guess\n")
+      if not IsValidGuess(guessed_letter):
+        print("Please make valid guess.")  # implement details on why guess is invalid
+      
+      break
